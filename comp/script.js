103cd7f651ce5e17098860503c55d8d98a57d265
RandomFella <amirkivam@gmail.com>
2025-07-07 16:41:33 -0500

    Update script.js

--- comp/script.js
+++ comp/script.js
@@ -67,3 +67,23 @@ a.addEventListener("click", () => {
 ah.addEventListener("click", () => {
   about.scrollIntoView({ behavior: "smooth" });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const reveals = document.querySelectorAll(".reveal");
+  const options = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  reveals.forEach((el) => {
+    observer.observe(el);
+  });
+});
